3fc957d6b047d5e6dd5b51141ff8147ea15e7030
Gabrielle Nyawera <nyaweragabby@gmail.com>
2026-09-17 21:18:18 +0200

    fix: sticky left panel positioning in interests section

--- src/components/sticky-interests.tsx
+++ src/components/sticky-interests.tsx
@@ -67,9 +67,9 @@ export function StickyInterests() {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row">
+    <div className="relative flex flex-col sm:flex-row">
       {/* Left panel - fixed on desktop */}
-      <div className="sticky top-0 flex h-auto flex-col justify-start py-16 pl-5 pr-8 sm:h-screen sm:w-[35%] sm:py-16 sm:pl-8 lg:pl-16 lg:pr-12">
+      <div className="sticky top-[72px] z-10 flex h-auto flex-col justify-start py-10 pl-5 pr-8 sm:sticky sm:h-screen sm:w-[35%] sm:py-16 sm:pl-8 lg:pl-16 lg:pr-12">
         <h2 className="font-serif text-[2.5rem] font-light leading-[1.05] tracking-tight sm:text-5xl md:text-6xl">
           What I&apos;m
           <br />
